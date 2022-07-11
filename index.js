5c6003c0fee3bc939567d9260f35fbedf2ed966b
L0RRIX <109044047+L0RRIX@users.noreply.github.com>
2022-07-11 17:26:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,4 +54,48 @@ client.on('message', message => {
     }
 })
 
+client.on("messageCreate", message => {
+    if (message.content.startsWith("?ban")) {
+        let utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                let embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
 
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+
+
+client.on("messageCreate", message => {
+    if (message.content.startsWith("?clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        let count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("Inserisci un numero valido")
+        }
+        if (count > 100) {
+            return message.channel.send("Non puoi cancellare più di 100 messaggi")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati").then(msg => {
+            setTimeout(() => msg.delete(), 5000)
+        })
+    }
+})
